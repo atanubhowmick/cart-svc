d3331744c7a7d92d8e8ee3eed53131bb751d4522
Atanu Bhowmick <mail2atanu007@gmail.com>
2020-05-24 18:23:29 +0530

    Build number property added

--- src/main/java/dev/atanu/ecom/cart/CartServiceApplication.java
+++ src/main/java/dev/atanu/ecom/cart/CartServiceApplication.java
@@ -1,10 +1,16 @@
 package dev.atanu.ecom.cart;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import dev.atanu.ecom.cart.dto.ProjectBuildDetails;
 
 @EnableEurekaClient
 @EnableFeignClients
@@ -12,8 +18,23 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 public class CartServiceApplication {
 
+	@Value("${info.app.version}")
+	private String projectVersion;
+	
+	@Value("${build.number}")
+	private String buildNumber;
+
+	private static final Logger logger = LoggerFactory.getLogger(CartServiceApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CartServiceApplication.class, args);
+	}
+	
+	@Bean
+	public ProjectBuildDetails getBuildNumber() {
+		ProjectBuildDetails details = new ProjectBuildDetails(projectVersion, buildNumber);
+		logger.info("Project Build Details : {}", details);
+		return details;
 	}
 
 }
